000a0c0bc35174c6bc9ff9bd95131594f4c70fbf
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-08-25 11:44:17 +0300

    added route function for the login form

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,41 +1,41 @@
-# from flask import render_template,redirect,url_for,flash,request
-# from . import auth
-# from ..models import User
-# from flask_login import login_user,logout_user,login_required
-# from . import views,forms
-# from .forms import RegistrationForm,LoginForm
-# from ..import db
+from flask import render_template,redirect,url_for,flash,request
+from . import auth
+from ..models import User
+from flask_login import login_user,logout_user,login_required
+from . import views,forms
+from .forms import RegistrationForm,LoginForm
+from ..import db
 
-# @auth.route('/login',methods = ['GET','POST'])
-# def login():
-#     login_form = LoginForm()
-#     if login_form.validate_on_submit():
-#         user = User.query.filter_by(email = login_form.email.data).first()
-#         if user is not None and user.verify_password(login_form.password.data):
-#             login_user(user,login_form.remember.data)
-#             return redirect (url_for('main.index'))
-#         flash('Invalid username or Password')
-#         title = " Quotes login"
-#     return render_template('auth/login.html',login_form = login_form)   
+@auth.route('/login',methods = ['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect (url_for('main.index'))
+        flash('Invalid username or Password')
+        title = " Quotes login"
+    return render_template('auth/login.html',login_form = login_form)   
 
-# @auth.route('/register',methods = ["GET","POST"])
-# def register():
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         user = User(email = form.email.data,username = form.username.data,password = form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data,username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
         
-#         # mail_message("Welcome to Quotes","email/welcome_user",user.email,user=user)
+        # mail_message("Welcome to Quotes","email/welcome_user",user.email,user=user)
     
-#         return redirect(url_for('auth.login'))
-#         title = 'New Account'
+        return redirect(url_for('auth.login'))
+        title = 'New Account'
 
-#     return render_template('auth/register.html',register_form = form)   
+    return render_template('auth/register.html',register_form = form)   
 
-# @auth.route('/logout')
-# @login_required
-# def logout():
-#     logout_user()
-#     flash('You have successfully logged out')
-#     return redirect(url_for("main.index"))
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have successfully logged out')
+    return redirect(url_for("main.index"))
